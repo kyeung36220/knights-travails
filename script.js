67feaebbe8029e4076d5c9e5710a3b865cd1098a
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2025-01-20 22:31:02 -0500

    Fixed so now is 100% accurate

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ class Cell {
         this.cellHistory = [...previousCellArray, this.cellCoords]
         this.possibleMoves = []
     }
-
     findAllPossibleNextCells() {
         const allPoss = []
         const invalidCells = this.cellHistory
         const cellXaxis = this.cellCoords[0]
         const cellYaxis = this.cellCoords[1]
-
         if (isCellInBoard([cellXaxis - 2, cellYaxis - 1], invalidCells)) {
             allPoss.push([cellXaxis - 2, cellYaxis - 1])
         }
@@ -37,7 +35,6 @@ class Cell {
         }
         return allPoss
     }
-
     get history() {
         return this.cellHistory
     }
@@ -45,20 +42,16 @@ class Cell {
         return this.cellCoords
     }
 }
-
 function findShortestPath(startCell, endCell) {
     const startingPosition = new Cell([], startCell)
-    let found = false // halts recursion as soon as solution is found
-    let shortestPath = [1,2,3,4,5,6,7,8] // numbers are just filler to be larger than largest path
+    let shortestPath = [] 
 
     // checks if start and end are at the same place
-    if (JSON.stringify(startCell) === JSON.stringify(endCell)) {
-        shortestPath = []
+    if (JSON.stringify(startCell) != JSON.stringify(endCell)) {
+        findShortestPathRecursion(startingPosition)
     }
-
-    findShortestPathRecursion(startingPosition)
-    shortestPath.push(endCell) // recursion stops when cell is found in next iteration so I just tack this on at the end
-
+    shortestPath.push(endCell) // add the end cell at the end because recursion doesn't add it
+    
     let returnedString = `You made it in ${shortestPath.length - 1} moves! Here's your path:\n`
     for (let i = 0; i < shortestPath.length; i++) {
         returnedString += `[${shortestPath[i]}]`
@@ -73,36 +66,29 @@ function findShortestPath(startCell, endCell) {
         if (possibleCells.length === 0) { // if no possible cells to move to then stop
             return
         }
-
-        if (cell.history.length >= 7) { // if 7 moves are used up then stop
+        if (cell.history.length >= 7) { // if 7 moves are used up then stop (7 because that is the maximum a knight needs to move to any square)
             return
         }
-
         // if possible cell contains the end cell, then it is a valid path
         for (let i = 0; i < possibleCells.length; i++) {
             if (JSON.stringify(possibleCells[i]) === JSON.stringify(endCell)) {
-                shortestPath = cell.history
-                found = true
+                if (cell.history.length < shortestPath || shortestPath.length === 0) {
+                    shortestPath = cell.history
+                }
+                return
             }
         }
-
         // recursively call this function while creating a new cell with it's path history
         for (let i = 0; i < possibleCells.length; i++) {
-            if (found === false) {
-                findShortestPathRecursion(new Cell([...cell.history], possibleCells[i]))
-            }
+            findShortestPathRecursion(new Cell([...cell.history], possibleCells[i]))
         }
-        return cell.history
     }
 }
-
 function isCellInBoard(cell, invalidCells) {
-
     // if outside board then is invalid
     if (cell[0] > 7 || cell[0] < 0 || cell[1] > 7 || cell[1] < 0) {
         return false
     }
-
     // if cell has already been used, then it is invalid
     let found = false
     for (let i = 0; i < invalidCells.length; i++) {
@@ -117,4 +103,7 @@ function isCellInBoard(cell, invalidCells) {
     return true
 }
 
+function randomCoords() {
+    return [Math.floor(Math.random() * 7), Math.floor(Math.random() * 7)]
+}
 console.log(findShortestPath([7,7], [0,0]))
